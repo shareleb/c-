4c8301857ebf8f42c9b11fd2deac48ed64237002
shareleb <2524214961@qq.com>
2019-10-20 09:52:45 +0800

    01

---
+++ c++学习(1)/c++学习(1)/01.hello.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>//标准输入输出流 in输入 out 输出
+//using namespace std;  //使用命名空间std;
+//int main() {
+//	//cout标准的输出
+//	//左移运算符;符号重载.
+//	//endl结束换行.
+//	cout << "hello world" <<123456<< endl;
+//	system("pause");//阻塞功能
+//	return EXIT_SUCCESS; //返回正常退出
+//}
+
+#include<iostream>
