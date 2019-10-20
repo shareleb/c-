34553e19dfe0ffb29a7f3802e5e9fe23df9eed9c
shareleb <2524214961@qq.com>
2019-10-20 09:54:26 +0800

    02

---
+++ c++学习(1)/c++学习(1)/02.双冒号作用域运算符.cpp
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;  
+//int atk = 200;
+//
+//void test01() {
+//	int atk = 100;
+//	cout << "攻击力为:" << atk << endl;
+//	//双冒号 作用域运算符    ::全局作用域;
+//	cout << "攻击力为:" << ::atk << endl;
+//
+//}
+//int main() {
+//	int n;
+//	cin >> n;
+//	system("pause");//阻塞功能
+//	return EXIT_SUCCESS; //返回正常退出
+//}
