5203a8f1ca32e06b780700c184ce27abf7c7831d
shareleb <2524214961@qq.com>
2019-10-21 14:36:31 +0800

    07

---
+++ c++学习(1)/c++学习(1)/07c语言中的const默认是外部链接.c
@@ -0,0 +1,11 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//
+//	                    //c++中的const默认是内部链接,只能在本文件内使用,   extern来提高作用域.两面都加extern.
+//	extern const int a;//告诉编译器a在外部.   
+//	printf("a=%d\n", a);
+//	system("pause");
+//	return EXIT_SUCCESS;
+//}
