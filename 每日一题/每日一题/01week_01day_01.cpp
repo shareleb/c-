a98ab8e05be016d3af7d9779e631a16c9b1d362c
shareleb <2524214961@qq.com>
2019-11-12 21:24:30 +0800

    牛客网

---
+++ 每日一题/每日一题/01week_01day_01.cpp
@@ -0,0 +1,29 @@
+//
+//
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int sum = 0;
+//	vector<int>v;
+//	v.resize(n * 3);
+//	for (int i = 0; i < (3*n); i++)
+//	{
+//		cin >> v[i];
+//	}
+//	sort(v.begin(), v.end());
+//	for (int i = n; i <= 3 * n - 2; i += 2)
+//	{
+//		sum += v[i];
+//	}
+//	cout << sum;
+//	return 0;
+//
+//}
+//
