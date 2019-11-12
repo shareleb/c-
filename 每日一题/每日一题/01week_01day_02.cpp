c4743d2ec3939ddaf9a612c0f4c885c4fdb5bf03
shareleb <2524214961@qq.com>
2019-11-12 21:25:48 +0800

    牛客网

---
+++ 每日一题/每日一题/01week_01day_02.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	string s1;
+//	string s2;
+//	getline(cin, s1);
+//	getline(cin, s2);
+//	int hashtable[256] = { 0 };
+//	int i;
+//	for (i = 0; i < s2.size(); i++)
+//	{
+//		hashtable[s2[i]]++;
+//	}
+//	string tmp;
+//	for (i = 0; i < s1.size(); i++)
+//	{
+//		if (hashtable[s1[i]] == 0)
+//		{
+//			tmp += s1[i];
+//		}
+//	}
+//	cout << tmp << endl;
+//
+//
+//	system("pause");
+//	return 0;
+//}
