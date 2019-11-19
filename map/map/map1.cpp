99478cb172b34fac0a62e850d98b045f01428bc3
shareleb <2524214961@qq.com>
2019-11-19 22:25:33 +0800

    map1.cpp

---
+++ map/map/map1.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//#include<map>
+//using namespace std;
+//#include<functional>
+//#include<set>
+//set 每个里面为一个单值。
+//set没有【】
+//默认从小到大排列
+//可以用范围for
+//平衡搜索树
+//
+//int main()
+//{
+//	int a[5] = { 5,2,3,1,4 };
+//	set<int>s(a,a+5);
+//	set<int>::iterator s1 = s.begin();
+//	s1++;
+//	s.insert(10);
+//
+//	cout << *s1 << endl;
+//	for (auto &a : s)
+//	{
+//		cout << a << endl;
+//	}
+//	
+//
+//
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//
+//}
