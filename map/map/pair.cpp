6629f3566e2e2ce8d4b5aaa8b930508ba6c86e1e
shareleb <2524214961@qq.com>
2019-11-19 22:26:22 +0800

    pair.cpp

---
+++ map/map/pair.cpp
@@ -0,0 +1,40 @@
+//#include<iostream>
+//#include<map>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	pair<int, int>p;
+//	p.first = 1;
+//	p.second = 2;
+//
+//	pair<int, int>p1;
+//	p1.first = 10;
+//	p1.second = 20;
+//	p.swap(p1);
+//
+//
+//	cout << "p1.first=" << p1.first << endl;
+//	cout << "p1.second=" << p1.second << endl;
+//
+//
+//	///////////////////////////////////////
+//	map<int, int>m;
+//	
+//	m.insert(pair<int,int>(2, 4));
+//	m.insert(p1);
+//	map<int, int>::iterator mi = m.begin();
+//	mi++;
+//	cout<<mi->first<<endl;
+//	cout << mi->second << endl;
+//
+//	map<int, int>::iterator mm=m.find(2);
+//	cout << mm->first << endl;
+//	cout << mm->second << endl;
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
