5a2a6c047c903568706b8de35d2f0e0c45d0b3d3
shareleb <2524214961@qq.com>
2019-11-24 23:14:00 +0800

    11

---
+++ 每日一题/每日一题/02week_11day_02.cpp
@@ -0,0 +1,32 @@
+//#include<iostream>
+//#include<vector>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	int a;
+//	cin >> a;
+//	int max = 0;
+//	int tmp = 0;
+//	for (int i = 0; i < 8; i++)
+//	{
+//		int p = (a >> i) & 1;
+//		if (p == 1)
+//		{
+//			tmp++;
+//			if (tmp > max)
+//			{
+//				max = tmp;
+//			}
+//		}
+//		else
+//		{
+//			tmp = 0;
+//		}
+//	}
+//
+//	cout << max << endl;
+//	return 0;
+//
+//}
