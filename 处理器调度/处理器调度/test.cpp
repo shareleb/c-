a37f4bca57c2af1b8b8981129d73dc0703fc2a6a
shareleb <2524214961@qq.com>
2019-11-26 13:18:41 +0800

    01

--- 处理器调度/处理器调度/test.cpp
+++ 处理器调度/处理器调度/test.cpp
@@ -1,41 +1,41 @@
-#include<iostream>
-#include<iomanip>
-#include<fstream>
-using namespace std;
-
-int main()
-{
-
-	ofstream osf;
-	osf.open(".//a.txt", ios::out | ios::trunc);
-	osf << "姓名：" << endl;
-	osf << "性别：" << endl;
-	osf << "年龄：" << endl;
-	if (osf.is_open())
-	{
-		cout << "打开成功" << endl;
-
-	}
-	else
-	{
-		cout << "打开失败" << endl;
-	}
-	long long index = osf.tellp();
-	cout << index << endl;
-		
-	ifstream ifs;
-
-	ifs.open("a.txt", ios::in);
-	char buf[1024];
-	while (ifs >> buf)
-	{
-		cout << buf;
-	}
-
-	return 0;
-
-
-
-
-
-}
+//#include<iostream>
+//#include<iomanip>
+//#include<fstream>
+//using namespace std;
+//
+//int main()
+//{
+//
+//	ofstream osf;
+//	osf.open(".//a.txt", ios::out | ios::trunc);
+//	osf << "姓名：" << endl;
+//	osf << "性别：" << endl;
+//	osf << "年龄：" << endl;
+//	if (osf.is_open())
+//	{
+//		cout << "打开成功" << endl;
+//
+//	}
+//	else
+//	{
+//		cout << "打开失败" << endl;
+//	}
+//	long long index = osf.tellp();
+//	cout << index << endl;
+//		
+//	ifstream ifs;
+//
+//	ifs.open("a.txt", ios::in);
+//	char buf[1024];
+//	while (ifs >> buf)
+//	{
+//		cout << buf;
+//	}
+//
+//	return 0;
+//
+//
+//
+//
+//
+//}
