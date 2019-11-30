651dd05000c4754ba13d4be3e1a4e21323699ce3
shareleb <2524214961@qq.com>
2019-11-30 09:31:53 +0800

    32

---
+++ 每日一题/每日一题/03week_15day_01.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int a;
+//	while (cin >> a)
+//	{
+//		int count = 0;
+//		for (int i = 0; i < 32; i++)
+//		{
+//			int p = (a >> i) & 1;
+//			if (p == 1)
+//			{
+//				count++;
+//			}
+//		}
+//		cout << count << endl;
+//
+//
+//
+//
+//
+//	}
+//
+//
+//
+//	return 0;
+//}
