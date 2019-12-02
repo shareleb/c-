38cad1079616e8ef41f1ab6590e6dba0e85ca681
shareleb <2524214961@qq.com>
2019-12-02 21:22:12 +0800

    23

---
+++ 每日一题/每日一题/11月20日_day13_01 .cpp
@@ -0,0 +1,36 @@
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//#include<list>
+//#include<deque>
+//#include<map>
+//#include<set>
+//#include<ctype>
+//using namespace std;
+////find string 返回的是下标   map
+////set 有
+////vector  没有find
+////list没有find
+////deque 没有find
+////map返回对应的迭代器
+////set返回对应的迭代器
+//int main()
+//{
+//	
+//	string s;
+//	getline(cin, s);
+//	int index;
+//	string tmp;
+//	
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		
+//	}
+//		
+//	
+//
+//	
+//
+//	system("pause");
+//	return 0;
+//}
