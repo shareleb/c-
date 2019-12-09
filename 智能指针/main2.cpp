137648a55114a84fecf4f452f1f74ac1502ed47e
shareleb <2524214961@qq.com>
2019-12-09 23:19:26 +0800

    12

---
+++ 智能指针/main2.cpp
@@ -0,0 +1,28 @@
+//#include<iostream>
+//#include<vector>
+//#include<memory>
+//using namespace std;
+//
+////
+//class test
+//{
+//public:
+//	int m_a;
+//};
+//int main()
+//{
+//	auto_ptr<int>ap(new int[5]);    //auto_ptr采用托管方式，
+//	auto_ptr<test>apt(new test);
+//	*ap = 5;
+//	apt->m_a = 6;
+//
+//
+//	cout << *ap << endl;
+//	cout << apt->m_a << endl;
+//
+//	auto_ptr<int>ap2(ap); //拷贝构造
+//	cout << *ap2 << endl;
+//
+//	system("pause");
+//	return 0;
+//}
