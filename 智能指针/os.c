5493cb3eb8189a9ea8a031d9bbd0bae528893d92
shareleb <2524214961@qq.com>
2019-12-12 22:43:20 +0800

    21

---
+++ 智能指针/os.c
@@ -0,0 +1,22 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//
+//int main()
+//{
+//
+//	int i = 0;
+//	int * p = NULL;
+//	while (1)
+//	{
+//		p = (int*)malloc(100 * 1024 * 1024);
+//		if (p == NULL)
+//		{
+//			return -1;
+//		}
+//		i = i + 1;
+//		printf("第%d次申请成功100M内存！\n", i);
+//	}
+//	system("pasue");
+//	return 0;
+//}
