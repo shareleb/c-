e7c4af2821a92d66b07215011200fcc77d10d613
shareleb <2524214961@qq.com>
2019-12-21 22:46:55 +0800

    90

---
+++ 三角矩阵/三角矩阵/test.cpp
@@ -0,0 +1,36 @@
+//#include<iostream>
+//using namespace std;
+//#include<string>
+//class a
+//{
+//public:
+//	
+//	virtual	void speak()    
+//	{
+//		cout << "a speak" << endl;
+//	}
+//};
+//
+//class bbbb : public a
+//{
+//public:
+//
+//	void speak()
+//	{
+//		cout << "b speak" << endl;
+//	}
+//};
+//void test01(a & aa)  //Ôç°ó¶¨£¬¾²Ì¬Áª±à        ¶àÌ¬ Íí°ó¶¨£¬¶¯Ì¬Á¬±ß
+//{
+//	aa.speak();
+//}
+//
+//
+//int main()
+//{
+//	bbbb bb;
+//	test01(bb);
+//
+//	system("pause");
+//	return 0;
+//}
