4eb4bb529d3069b603089377779662e1ea27516a
shareleb <2524214961@qq.com>
2020-01-01 15:59:46 +0800

    命名空间

---
+++ c++复习/c++基础/c++基础/命名空间.cpp
@@ -0,0 +1,46 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//namespace leb
+//{
+//	int a = 10;
+//	int b = 20;
+//	int func(int a, int b)
+//	{
+//		return a + b;
+//	}
+//	namespace l
+//	{
+//		int d = 10;
+//		int func(int a, int b)
+//		{
+//			return a + b;
+//		}
+//	}
+//}
+//namespace leb
+//{
+//	int c = 10;
+//	
+//}
+//
+//using  namespace  leb;
+//
+//int add(int a = 10, int b = 20)
+//{
+//	return a + b;
+//}
+//
+//int main()
+//{
+//	int aa = 10;
+//	int bb = 20;
+//	bb=func(aa, bb);
+//	cout << bb << endl;
+//	cout << add() << endl;
+//	cout <<l:: d << endl;
+//	system("pause");
+//	return 0;
+//
+//}
