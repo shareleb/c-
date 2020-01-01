ae97f468eea5c473559f69827a8fb9c26a034e08
shareleb <2524214961@qq.com>
2020-01-01 15:58:06 +0800

    函数重载

---
+++ c++复习/c++基础/c++基础/函数重载.cpp
@@ -0,0 +1,27 @@
+//#include<iostream>
+//using namespace std;
+//
+//int add(int a = 10)
+//{
+//	return a ;
+//}
+//extern "C" {
+//	int add(int a)
+//	{
+//		return a;
+//	}
+//}
+//void TestFunc(int a = 10)
+//{
+//	cout << "void TestFunc(int)" << endl;
+//}
+//void TestFunc(int a)
+//{
+//	cout << "void TestFunc(int)" << endl;
+//}
+//int main()
+//{
+//	TestFunc();
+//	system("pause");
+//	return 0;
+//}
