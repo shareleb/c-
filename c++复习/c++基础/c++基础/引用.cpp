73baec558b0361665f9c34ce4895942c373d7442
shareleb <2524214961@qq.com>
2020-01-01 16:00:23 +0800

    引用

---
+++ c++复习/c++基础/c++基础/引用.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//using namespace std;
+//
+//
+//
+//int main()
+//{
+//	int a = 10;
+//	int & ra = a;  //表示是a的别名，也表示是a的一个引用
+//	//引用-》 变量的别名
+//	int b = 29;
+//	int &rb = b;
+//	ra = b;//不是改变引用，而是给引用从新赋值。
+//	const	int&c = 32;
+//	cout << typeid(nullptr).name();
+//
+//	system("pause");
+//	return 0;
+//}
